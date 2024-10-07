f8fc1266fa9cbd344dc728997bebd56e4ee98aa7
Frida Lien <fridalie@uio.no>
2024-10-07 19:58:20 +0200

    Figures, documentation, and minor changes

--- LinearRegression/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base_model import BaseModel
-from .ordinary_least_squares import OrdinaryLeastSquares
-from .ridge_regression import RidgeRegression
-from .lasso_regression import LassoRegression
